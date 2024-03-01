cdd4baa048ae3c187f3588b2d68b6b85ead57233
Sarmad rafique <sarmadrafique040@gmail.com>
2024-03-01 16:31:47 +0500

    streamlit display media

---
+++ 5-Streamlit/1-basic-app/media.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.subheader("Youtube video: ")
+
+st.video("https://youtu.be/UU1WVnMk4E8?si=UWvYARiusKs40Jks")
+
+st.subheader("Image: ")
+
+st.image("./prompt-lifecycle.jpg")
